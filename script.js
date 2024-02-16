fa36c2c786d1ed22871910014903eeed18709b46
ShantanuThakare99 <155950845+ShantanuThakare99@users.noreply.github.com>
2024-02-16 14:11:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ async function fetchWeatherData(cityName){
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1ab300d4cd6ab7f40af580e84143a631`;
 	const response = await fetch(url)
 	const data = await response.json();
-	console.log(data)
+	let city = document.getElementById("city");
+	city.innerHTML = data.name;
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML = data.wind.speed + "km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+"g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
+	console.log(data);
 }
 
 function fetchCity() {
